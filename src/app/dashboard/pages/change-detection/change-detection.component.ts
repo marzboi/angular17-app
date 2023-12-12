3ec189d7fff2628b362392f02c5e246d2e6a6ac1
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-12-12 20:12:42 +0100

    create examples of defer options

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,10 +1,50 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './change-detection.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title [title]="currentFramework()" />
+
+    <pre>{{ frameworkAsSignal() | json }}</pre>
+
+    <pre>{{ frameworkAsProperty | json }}</pre>
+  `,
   styles: ``,
 })
-export default class ChangeDetectionComponent {}
+export default class ChangeDetectionComponent {
+  public currentFramework = computed(
+    () => `Change Detection = ${this.frameworkAsSignal().name}`
+  );
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+      // this.frameworkAsProperty.name = 'React';
+
+      this.frameworkAsSignal.update((value) => ({
+        ...value,
+        name: 'React',
+      }));
+
+      console.log('Hecho');
+    }, 2000);
+  }
+}
