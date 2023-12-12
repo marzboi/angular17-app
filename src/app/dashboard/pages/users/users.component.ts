8579b834bdc717c0ed1c428c43ec18536f60afad
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-12-12 22:27:55 +0100

    Create user service

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { UsersService } from '@services/users.service';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TitleComponent, RouterModule],
   templateUrl: './users.component.html',
   styles: ``,
 })
-export default class UsersComponent {}
+export default class UsersComponent {
+  public usersService = inject(UsersService);
+}
