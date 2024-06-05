3c6645a0b29f7e64c6b45d787752a237566295be
Marco Fratini <marcofratini@outlook.com>
2024-06-05 10:18:14 -0400

    Craete UI component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
-import {} from '@angular/common/http';
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,6 +20,7 @@ export const appConfig: ApplicationConfig = {
       })
     ),
 
-    importProvidersFrom(HttpClientModule),
+    // importProvidersFrom(HttpClientModule),
+    provideHttpClient(withInterceptorsFromDi()),
   ],
 };
---
+++ src/app/dashboard/pages/input-output/ui/product-card/product-card.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-card.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProductCardComponent {}
