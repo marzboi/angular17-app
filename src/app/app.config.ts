6814e0b83450c0d99c6ead177f609311ac458403
Marco Fratini <marcofratini@outlook.com>
2024-06-05 08:58:18 -0400

    Update to version 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
